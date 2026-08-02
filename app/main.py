1eb828dc19aafb3aaaf1d0211432b36c9e2ce9d7
Darakhasha Shahin <darakhasha21@gmail.com>
2026-08-02 18:43:32 +0530

    Initial FastAPI dummy API setup

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI(
+    title="Taxi Demand Prediction API",
+    description="Dummy API for Pipeline 4 development",
+    version="1.0"
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Taxi Demand API is Running"
+    }
+
+
+@app.post("/predict")
+def predict():
+    prediction = random.randint(100, 300)
+
+    return {
+        "predicted_demand": prediction
+    }
