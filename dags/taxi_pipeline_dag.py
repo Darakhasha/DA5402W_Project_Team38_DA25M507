6b59026f5fbb28cd1d6ffd19a21456edda3069a1
Darakhasha Shahin <darakhasha21@gmail.com>
2026-08-23 20:09:07 +0530

    Bug fix airflow

--- dags/taxi_pipeline_dag.py
+++ dags/taxi_pipeline_dag.py
@@ -27,26 +27,37 @@ with DAG(
     # 1. Pull raw Parquet dataset from MinIO via DVC
     task_dvc_pull = BashOperator(
         task_id='dvc_pull_raw_data',
-        bash_command='dvc pull data/raw/yellow_tripdata_2024-01.parquet.dvc || true',
+        bash_command='''
+            cd /opt/airflow && \
+            mkdir -p data/raw data/processed && \
+            dvc pull data/raw/yellow_tripdata_2024-01.parquet.dvc || true
+        ''',
         env=dvc_env,
     )
 
     # 2. Clean raw data and engineer features
     task_clean_features = BashOperator(
         task_id='clean_and_engineer_features',
-        bash_command='python scripts/process_data.py --input data/raw/yellow_tripdata_2024-01.parquet --output data/processed/taxi_demand_features.parquet',
+        bash_command='''
+            cd /opt/airflow && \
+            mkdir -p data/processed && \
+            python scripts/process_data.py --input data/raw/yellow_tripdata_2024-01.parquet --output data/processed/taxi_demand_features.parquet
+        ''',
     )
 
-    # 3. Run Spark batch aggregation on Kafka stream
+
+
+   # 3. Run Spark batch aggregation on Kafka stream
     task_spark_batch = BashOperator(
         task_id='run_spark_batch_aggregation',
-        bash_command='python src/streaming_pipeline.py',
+        bash_command='cd /opt/airflow && python3 -m pip install --user duckdb && python src/streaming_pipeline.py',
     )
+
 
     # 4. Version final processed dataset with DVC and push to MinIO
     task_dvc_version = BashOperator(
         task_id='dvc_version_dataset',
-        bash_command='dvc add data/processed/taxi_demand_features.parquet && dvc push',
+        bash_command='cd /opt/airflow && dvc add data/processed/taxi_demand_features.parquet && dvc push',
         env=dvc_env,
     )
 
